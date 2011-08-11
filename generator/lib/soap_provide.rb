8a72a42af268701c4735dbc5d8fd686e6a184fcf
Felipe Besson <flpbesson@gmail.com>
2011-08-11 18:31:38 -0300

    generating soap-provides automatically

---
+++ generator/lib/soap_provide.rb
@@ -0,0 +1,50 @@
+require 'fileutils'
+include FileUtils
+
+module SOAP_Provide
+	module_function
+	def create_soap_provider id
+		cd "../petals/soap-provide"		
+		create_su_package id
+		create_sa_package id
+		cd "../../scripts"
+	end
+
+	def create_su_package id
+		mkdir_p "META-INF"
+		cp "../../workspace/SM#{id}.wsdl", "SM#{id}.wsdl"
+
+		su_jbi = File.open("../../resources/soap/provide/su-jbi.xml", "r").readlines.join.gsub('#{id}', id.to_s)
+		open_file_and_write "META-INF/jbi.xml", su_jbi	
+				
+		zip_j "su-SOAP-SM#{id}-provide.zip", "SM#{id}.wsdl"
+		zip "su-SOAP-SM#{id}-provide.zip", "META-INF"				
+	end
+
+	def create_sa_package id
+		mkdir_p "META-INF"
+		sa_jbi = File.open("../../resources/soap/provide/sa-jbi.xml", "r").readlines.join.gsub('#{id}', id.to_s)
+		open_file_and_write "META-INF/jbi.xml", sa_jbi	
+				
+		zip_j "sa-SOAP-SM#{id}-provide.zip", "su-SOAP-SM#{id}-provide.zip"
+		zip "sa-SOAP-SM#{id}-provide.zip", "META-INF"				
+	end
+	
+	def open_file_and_write file_name, content
+	    file = File.new(file_name, "w")
+	    file.puts content
+	    file.close
+	end
+
+	def zip zip_file, *args
+	  list = args.join " "
+	  `zip -qr0m #{zip_file} #{list} 2>/dev/null`
+	end
+
+	def zip_j zip_file, *args
+	  list = args.join " "
+	  `zip -qr0mj #{zip_file} #{list} 2>/dev/null`
+	end
+end
+
+		
