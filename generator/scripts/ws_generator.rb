1b8a23b084ee273354a2e8a369314b9f618f19e2
Felipe Besson <flpbesson@gmail.com>
2011-08-13 15:48:54 -0300

    cleaning workspace directory

--- generator/scripts/ws_generator.rb
+++ generator/scripts/ws_generator.rb
@@ -3,6 +3,9 @@ include FileUtils
 
 require '../lib/wsdl'
 require '../lib/adaptor'
+require '../lib/soap_provide'
+require '../lib/soap_consume'
+require '../lib/bpel'
 
 number_of_ws = ARGV[0].to_i
 
@@ -11,7 +14,16 @@ number_of_ws = ARGV[0].to_i
 	WSDL.generate_wsdl id
 	
 	#Creating web services implementation using wsdl2java
-	WSDL.implement_ws id		
+	WSDL.implement_ws id
+
+	#Creating petals provide assemblies
+	SOAP_Provide.create_soap_provider id
+
+	#Creating petals bpel assemblies
+	BPEL.create_bpel id
+
+	#Creating petals bpel assemblies
+	SOAP_Consume.create_soap_consume id
 end
 
 Adaptor.adapt_ws "../workspace/eu/choreos/services/.", number_of_ws
@@ -19,3 +31,5 @@ Adaptor.adapt_ws "../workspace/eu/choreos/services/.", number_of_ws
 #Start ws server
 cd "../workspace"
 `ant SM#{number_of_ws}PortTypeServer`
+
+
