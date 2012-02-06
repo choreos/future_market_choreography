1aa92d3aef352ee35a75a79396f90c7d09af798d
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2012-02-06 18:29:17 -0200

    SMRegistry Tomcat project

--- webServices/SMRegistry/src/main/java/eu/choreos/services/SMRegistryWS.java
+++ webServices/SMRegistry/src/main/java/eu/choreos/services/SMRegistryWS.java
@@ -8,36 +8,33 @@ import java.util.Set;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-@WebService(targetNamespace="http://smregistry.choreos.eu")
+@WebService(targetNamespace = "http://smregistry.choreos.eu")
 public class SMRegistryWS {
-	
+
 	private Set<String> supermarkets;
-	
-	public SMRegistryWS(){
+
+	public SMRegistryWS() {
 		supermarkets = new HashSet<String>();
 	}
-	
+
 	@WebMethod
-	public List<String> getList(){
+	public List<String> getList() {
 		return (List<String>) (new ArrayList<String>(supermarkets));
 	}
-	
+
 	@WebMethod
-	public String addSupermarket(String endpoint){
+	public String addSupermarket(String endpoint) {
 		supermarkets.add(endpoint);
-		
+
 		return "OK";
 	}
-	
+
 	@WebMethod
-	public String removeSupermarket(String endpoint){
-		if(supermarkets.contains(endpoint)) {
+	public String removeSupermarket(String endpoint) {
+		if (supermarkets.contains(endpoint)) {
 			supermarkets.remove(endpoint);
 			return "OK";
-		}
-		else
+		} else
 			return "Endpoint not found";
-			
 	}
-	
 }
