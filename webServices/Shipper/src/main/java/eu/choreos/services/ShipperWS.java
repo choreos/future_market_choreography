c3a50aec530f2d4ece859b8367a184290441e731
Paulo Moura <pbmoura@gmail.com>
2012-02-09 18:34:03 -0200

    implemented setDelivery()

--- webServices/Shipper/src/main/java/eu/choreos/services/ShipperWS.java
+++ webServices/Shipper/src/main/java/eu/choreos/services/ShipperWS.java
@@ -1,21 +1,65 @@
 package eu.choreos.services;
 
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.apache.xmlbeans.XmlException;
+
+import eu.choreos.DeliveryInfo;
+import eu.choreos.PurchaseInfo;
+import eu.choreos.vv.clientgenerator.WSClient;
+import eu.choreos.vv.exceptions.FrameworkException;
+import eu.choreos.vv.exceptions.InvalidOperationNameException;
+import eu.choreos.vv.exceptions.WSDLException;
+
 @WebService
-public class ShipperWS {
+public class ShipperWS  {
 	
 	HashMap<Integer, String> deliveries = new HashMap<Integer, String>();
 	
+	long id;
+	
 	@WebMethod
-	public String setDelivery(String id, String zipcode){
-		deliveries.put(Integer.parseInt(id), zipcode);
-		return "ok";
+	public String setDelivery(PurchaseInfo purchaseinfo){
+		//this.start();
+		WSClient wscustomer;
+		try {
+			wscustomer = new WSClient(purchaseinfo.getCustomer().getEndpoint());
+			
+			DeliveryInfo deliveryInfo = new DeliveryInfo();
+			deliveryInfo.setPurchase(purchaseinfo);
+			deliveryInfo.setId(""+id++);
+			deliveryInfo.setDate(new Date().toString());
+			
+			
+			
+			return wscustomer.request("receiveShipmentData", deliveryInfo.getItem("arg0")).getChild("return").getContent();
+		} catch (WSDLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XmlException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FrameworkException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidOperationNameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "erro";
 	}
 	
 	@WebMethod
@@ -43,12 +87,6 @@ public class ShipperWS {
 		return c1;
 	}
 	
-}
-
-class ReceiveShipperDataCaller extends Thread {
 	
-	public void run() {
-		
-	}
-
+	
 }
