7e6cdb25ecaef3889e7be73837764bee61663cfa
Paulo Moura <pbmoura@gmail.com>
2012-02-23 16:30:29 -0200

    makePurchase fixed

--- webServices/customer/src/main/java/br/usp/ime/futuremarket/CustomerImpl.java
+++ webServices/customer/src/main/java/br/usp/ime/futuremarket/CustomerImpl.java
@@ -19,11 +19,11 @@ public class CustomerImpl implements Customer {
     private FutureMarket futureMarket;
     private Shipper shipper;
     // <listID, <supermarket,<product>>>
-    private HashMap<String, HashMap<String, Set<String>>> customerProductLists;
+    private HashMap<String, HashMap<Supermarket, Set<String>>> customerProductLists;
     private long currentList = 1L;
 
     public CustomerImpl() {
-        customerProductLists = new HashMap<String, HashMap<String, Set<String>>>();
+        customerProductLists = new HashMap<String, HashMap<Supermarket, Set<String>>>();
 
         futureMarket = new FutureMarket();
         supermarkets = futureMarket.getClients(FutureMarket.SUPERMARKET_ROLE,
@@ -34,7 +34,7 @@ public class CustomerImpl implements Customer {
 
     @WebMethod
     public LowestPrice getLowestPriceForList(String[] products) {
-        HashMap<HashMap<String, Double>, String> supermarketsProductList = new HashMap<HashMap<String, Double>, String>();
+        HashMap<HashMap<String, Double>, Supermarket> supermarketsProductList = new HashMap<HashMap<String, Double>, Supermarket>();
         // gets prices from supermarkets
         for (Supermarket supermarket : supermarkets) {
             ProductPrice[] productPrices = supermarket.getPrices(products);
@@ -42,35 +42,35 @@ public class CustomerImpl implements Customer {
             for (ProductPrice productPrice : productPrices) {
                 productsMap.put(productPrice.getProduct(), productPrice.getPrice());
             }
-            supermarketsProductList.put(productsMap, supermarket.getWsdl());
+            supermarketsProductList.put(productsMap, supermarket);
         }
 
         String listId = "" + getListId();
-        customerProductLists.put(listId, new HashMap<String, Set<String>>());
+        customerProductLists.put(listId, new HashMap<Supermarket, Set<String>>());
         Double listPrice = 0d;
         // finds lowest prices
         for (String product : products) {
-            String endpoint = "";
+            Supermarket supermarket = null;
             Double lowestPrice = Double.MAX_VALUE;
             for (HashMap<String, Double> productsHash : supermarketsProductList.keySet()) {
                 Double price = productsHash.get(product);
                 if (price < lowestPrice) {
                     lowestPrice = price;
-                    endpoint = supermarketsProductList.get(productsHash);
+                    supermarket = supermarketsProductList.get(productsHash);
                 }
             }
-            addProduct(listId, endpoint, product);
+            addProduct(listId, supermarket, product);
             listPrice += lowestPrice;
         }
 
         return new LowestPrice(listId, listPrice);
     }
 
-    private void addProduct(String listId, String endpoint, String product) {
-        if (customerProductLists.get(listId).get(endpoint) == null) {
-            customerProductLists.get(listId).put(endpoint, new HashSet<String>());
+    private void addProduct(String listId, Supermarket supermarket, String product) {
+        if (customerProductLists.get(listId).get(supermarket) == null) {
+            customerProductLists.get(listId).put(supermarket, new HashSet<String>());
         }
-        customerProductLists.get(listId).get(endpoint).add(product);
+        customerProductLists.get(listId).get(supermarket).add(product);
     }
 
     private synchronized long getListId() {
@@ -84,18 +84,18 @@ public class CustomerImpl implements Customer {
 
     @WebMethod
     public PurchaseInfo[] makePurchase(String listId, CustomerInfo customerInfo) {
-        HashMap<String, Set<String>> purchaseLists = customerProductLists.get(listId);
+        HashMap<Supermarket, Set<String>> purchaseLists = customerProductLists.get(listId);
         List<PurchaseInfo> result = new ArrayList<PurchaseInfo>();
 
         String[] products;
         PurchaseInfo purchaseInfo;
-        for (Supermarket supermarket : supermarkets) {
-            products = (String[]) purchaseLists.get(supermarket.getWsdl()).toArray();
+        for (Supermarket supermarket : purchaseLists.keySet()) {
+            products = purchaseLists.get(supermarket).toArray(new String[1]);
             purchaseInfo = supermarket.purchase(products, customerInfo);
             result.add(purchaseInfo);
         }
 
-        return (PurchaseInfo[]) result.toArray();
+        return result.toArray(new PurchaseInfo[1]);
     }
 
 }
