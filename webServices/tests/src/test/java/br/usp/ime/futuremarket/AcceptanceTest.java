5f7a33f3f55857d177433f1c2af96fcbcea675e0
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2012-02-24 20:57:43 -0200

    Refectoring acceptance tests

--- webServices/tests/src/test/java/br/usp/ime/futuremarket/AcceptanceTest.java
+++ webServices/tests/src/test/java/br/usp/ime/futuremarket/AcceptanceTest.java
@@ -9,6 +9,7 @@ import br.usp.ime.futuremarket.models.LowestPrice;
 
 public class AcceptanceTest {
     private static Customer customer;
+    private LowestPrice list;
 
     @BeforeClass
     public static void createClients() {
@@ -21,42 +22,42 @@ public class AcceptanceTest {
     // SM1
     public void product1LowestPriceShouldBe1() {
         final String[] products = { "product1" };
-        final LowestPrice price = customer.getLowestPriceForList(products);
+        list = customer.getLowestPriceForList(products);
 
-        assertEquals(1d, price.getPrice(), 0.01d);
+        assertEquals(1d, list.getPrice(), 0.01d);
     }
 
     @Test
     // SM2
     public void product2LowestPriceShouldBe2() {
         final String[] products = { "product2" };
-        final LowestPrice price = customer.getLowestPriceForList(products);
+        list = customer.getLowestPriceForList(products);
 
-        assertEquals(2d, price.getPrice(), 0.01d);
+        assertEquals(2d, list.getPrice(), 0.01d);
     }
 
     @Test
     // SM3
     public void product3LowestPriceShouldBe3() {
         final String[] products = { "product3" };
-        final LowestPrice price = customer.getLowestPriceForList(products);
+        list = customer.getLowestPriceForList(products);
 
-        assertEquals(3d, price.getPrice(), 0.01d);
+        assertEquals(3d, list.getPrice(), 0.01d);
     }
 
     @Test
     // All SMs
     public void shouldAddProductPrices() {
         final String[] products = { "product1", "product2", "product3" };
-        final LowestPrice price = customer.getLowestPriceForList(products);
+        list = customer.getLowestPriceForList(products);
 
-        assertEquals(6d, price.getPrice(), 0.01d);
+        assertEquals(6d, list.getPrice(), 0.01d);
     }
 
     @Test
     public void clientShouldBeAbleToPurchase() {
         final String[] products = { "product1", "product2", "product3" };
-        final LowestPrice list = customer.getLowestPriceForList(products);
+        list = customer.getLowestPriceForList(products);
 
         CustomerInfo customerInfo = new CustomerInfo();
         customerInfo.setName("JUnit test");
@@ -70,7 +71,7 @@ public class AcceptanceTest {
     @Test
     public void clientShouldBeAbleToGetShipmentData() {
         final String[] products = { "product1", "product2", "product3" };
-        final LowestPrice list = customer.getLowestPriceForList(products);
+        list = customer.getLowestPriceForList(products);
 
         CustomerInfo customerInfo = new CustomerInfo();
         customerInfo.setName("JUnit test");
