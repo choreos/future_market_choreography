e526a506cf584c66537ea41dc46c977153966ef3
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2012-02-28 16:39:03 -0300

    Logging each operation time

--- experiments/load-generator/src/main/java/br/usp/ime/futuremarket/LoadGenerator.java
+++ experiments/load-generator/src/main/java/br/usp/ime/futuremarket/LoadGenerator.java
@@ -13,20 +13,32 @@ import br.usp.ime.futuremarket.models.LowestPrice;
 public class LoadGenerator implements Runnable {
     private static Customer customer = null;
     private static int threadDuration;
-    private static final String LOG_FILE = "log.txt";
-    private static BufferedWriter out;
+
+    private static final String LOWEST_PRICE_LOG = "lowest_price.log";
+    private static final String PURCHASE_LOG = "purchase.log";
+    private static final String SHIPMENT_LOG = "shipment.log";
+
+    private static BufferedWriter lowestPrice;
+    private static BufferedWriter purchase;
+    private static BufferedWriter shipment;
 
     public LoadGenerator(final int threadNumber) {
         System.out.println("Thread " + threadNumber + " started.");
     }
 
-    private static BufferedWriter openLog() {
+    private static void openLogs() {
+        lowestPrice = openLog(LOWEST_PRICE_LOG);
+        purchase = openLog(PURCHASE_LOG);
+        shipment = openLog(SHIPMENT_LOG);
+    }
+
+    private static BufferedWriter openLog(final String filename) {
         FileWriter fstream = null;
 
         try {
-            fstream = new FileWriter(LOG_FILE);
+            fstream = new FileWriter(filename);
         } catch (IOException e) {
-            System.err.println("Error while opening " + LOG_FILE);
+            System.err.println("Error while opening " + filename);
             e.printStackTrace();
         }
 
@@ -35,7 +47,7 @@ public class LoadGenerator implements Runnable {
 
     public static void main(String[] args) {
         customer = getCustomer();
-        out = openLog();
+        openLogs();
 
         final int totalThreads = Integer.parseInt(args[0]);
         threadDuration = Integer.parseInt(args[1]);
@@ -45,7 +57,7 @@ public class LoadGenerator implements Runnable {
 
         runThreads(totalThreads, threadDuration);
 
-        closeLog();
+        closeLogs();
     }
 
     private static void runThreads(final int totalThreads, final int threadDuration) {
@@ -58,18 +70,19 @@ public class LoadGenerator implements Runnable {
 
         executor.shutdown();
         try {
-            while (!executor.awaitTermination(threadDuration + 30, TimeUnit.SECONDS)) {
-            }
+            while (!executor.awaitTermination(threadDuration + 60, TimeUnit.SECONDS));
         } catch (InterruptedException e) {
             executor.shutdownNow();
         }
     }
 
-    private static void closeLog() {
+    private static void closeLogs() {
         try {
-            out.close();
+            lowestPrice.close();
+            purchase.close();
+            shipment.close();
         } catch (IOException e) {
-            System.err.println("Error while closing file");
+            System.err.println("Error while closing log file");
             e.printStackTrace();
         }
     }
@@ -80,18 +93,25 @@ public class LoadGenerator implements Runnable {
                 FutureMarket.CUSTOMER_SERVICE, Customer.class);
     }
 
-    private long simulate() {
-        final long start = Calendar.getInstance().getTimeInMillis();
-
+    private void simulate() {
         final LowestPrice list = getLowestPriceList();
 
-        final PurchaseInfo[] purchaseInfos = customer
-                .makePurchase(list.getId(), new CustomerInfo());
-        verifyPurchase(purchaseInfos);
+        final PurchaseInfo[] purchaseInfos = purchase(list);
 
         requestShipmentData(purchaseInfos);
+    }
 
-        return Calendar.getInstance().getTimeInMillis() - start;
+    private PurchaseInfo[] purchase(final LowestPrice list) {
+        final long start = Calendar.getInstance().getTimeInMillis();
+        final PurchaseInfo[] purchaseInfos = customer
+                .makePurchase(list.getId(), new CustomerInfo());
+        final long end = Calendar.getInstance().getTimeInMillis();
+
+        logTime(purchase, start, end);
+
+        verifyPurchase(purchaseInfos);
+
+        return purchaseInfos;
     }
 
     private void verifyPurchase(PurchaseInfo[] purchaseInfos) {
@@ -100,9 +120,10 @@ public class LoadGenerator implements Runnable {
         }
     }
 
-    private static synchronized void logTime(final String time, final String duration) {
+    private static synchronized void logTime(final BufferedWriter out, final long start,
+            final long end) {
         try {
-            out.write(time + " " + duration + "\n");
+            out.write(start + " " + (end - start) + "\n");
         } catch (IOException e) {
             System.err.println("Error while writing to file");
             e.printStackTrace();
@@ -112,8 +133,15 @@ public class LoadGenerator implements Runnable {
     private void requestShipmentData(final PurchaseInfo[] purchaseInfos) {
         DeliveryInfo deliveryInfo;
 
+        long start;
+        long end;
         for (PurchaseInfo purchaseInfo : purchaseInfos) {
+            start = Calendar.getInstance().getTimeInMillis();
             deliveryInfo = customer.getShipmentData(purchaseInfo);
+            end = Calendar.getInstance().getTimeInMillis();
+
+            logTime(shipment, start, end);
+
             verifyDelivery(deliveryInfo);
         }
     }
@@ -126,7 +154,12 @@ public class LoadGenerator implements Runnable {
 
     private LowestPrice getLowestPriceList() {
         final String[] products = { "product1", "product2", "product3" };
+
+        final long start = Calendar.getInstance().getTimeInMillis();
         final LowestPrice list = customer.getLowestPriceForList(products);
+        final long end = Calendar.getInstance().getTimeInMillis();
+
+        logTime(lowestPrice, start, end);
 
         verifyList(list);
 
@@ -143,12 +176,8 @@ public class LoadGenerator implements Runnable {
     public void run() {
         final long start = Calendar.getInstance().getTimeInMillis();
 
-        long duration;
-        long now;
         while (Calendar.getInstance().getTimeInMillis() - start < threadDuration * 1000) {
-            now = Calendar.getInstance().getTimeInMillis();
-            duration = simulate();
-            logTime(Long.toString(now), Long.toString(duration));
+            simulate();
         }
     }
 }
