c420c3b2979c81732c90a46dc50d2e348481d911
Paulo Moura <pbmoura@gmail.com>
2012-03-19 12:00:14 -0300

    new scripts for time graphs

---
+++ experiments/Rscripts/chor_time_lowestPrice.R
@@ -0,0 +1,17 @@
+library("ggplot2")
+
+data <- list(read.table("data/chor/time/050/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/100/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/150/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/200/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/250/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/300/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/350/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/400/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/450/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/500/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/550/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/600/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/650/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/700/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/750/lowest_price.log",head=FALSE,sep=" "), read.table("data/chor/time/800/lowest_price.log",head=FALSE,sep=" ")) 
+
+
+banco = data.frame(Threads=c(050,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800),
+			 Mean_of_Delay = c(mean(data[[1]][2]),mean(data[[2]][2]),mean(data[[3]][2]),mean(data[[4]][2]),mean(data[[5]][2]),mean(data[[6]][2]),mean(data[[7]][2]),mean(data[[8]][2]),mean(data[[9]][2]),mean(data[[10]][2]),mean(data[[11]][2]),mean(data[[12]][2]),mean(data[[13]][2]),mean(data[[14]][2]),mean(data[[15]][2]),mean(data[[16]][2])),
+			 sigma=c(sd(data[[1]][2]),sd(data[[2]][2]),sd(data[[3]][2]),sd(data[[4]][2]),sd(data[[5]][2]),sd(data[[6]][2]),sd(data[[7]][2]),sd(data[[8]][2]),sd(data[[9]][2]),sd(data[[10]][2]),sd(data[[11]][2]),sd(data[[12]][2]),sd(data[[13]][2]),sd(data[[14]][2]),sd(data[[15]][2]),sd(data[[16]][2])))
+
+# Mean Variation
+	limits = aes(ymax = Mean_of_Delay + sigma, ymin= Mean_of_Delay - sigma)
+
+# Plot
+	png("png/chor_mean_lowestPrice.png")
+	p <- ggplot(banco, aes(x=Threads,y=Mean_of_Delay))
+	p + geom_point() + geom_errorbar(limits, width=50)
+	dev.off()
---
+++ experiments/Rscripts/orch_time_shipment.R
@@ -0,0 +1,17 @@
+library("ggplot2")
+
+data <- list(read.table("data/orch/time/050/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/100/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/150/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/200/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/250/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/300/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/350/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/400/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/450/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/500/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/550/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/600/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/650/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/700/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/750/shipment.log",head=FALSE,sep=" "), read.table("data/orch/time/800/shipment.log",head=FALSE,sep=" ")) 
+
+
+banco = data.frame(Threads=c(050,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800),
+			 Mean_of_Delay = c(mean(data[[1]][2]),mean(data[[2]][2]),mean(data[[3]][2]),mean(data[[4]][2]),mean(data[[5]][2]),mean(data[[6]][2]),mean(data[[7]][2]),mean(data[[8]][2]),mean(data[[9]][2]),mean(data[[10]][2]),mean(data[[11]][2]),mean(data[[12]][2]),mean(data[[13]][2]),mean(data[[14]][2]),mean(data[[15]][2]),mean(data[[16]][2])),
+			 sigma=c(sd(data[[1]][2]),sd(data[[2]][2]),sd(data[[3]][2]),sd(data[[4]][2]),sd(data[[5]][2]),sd(data[[6]][2]),sd(data[[7]][2]),sd(data[[8]][2]),sd(data[[9]][2]),sd(data[[10]][2]),sd(data[[11]][2]),sd(data[[12]][2]),sd(data[[13]][2]),sd(data[[14]][2]),sd(data[[15]][2]),sd(data[[16]][2])))
+
+# Mean Variation
+	limits = aes(ymax = Mean_of_Delay + sigma, ymin= Mean_of_Delay - sigma)
+
+# Plot
+	png("png/orch_mean_shipment.png")
+	p <- ggplot(banco, aes(x=Threads,y=Mean_of_Delay))
+	p + geom_point() + geom_errorbar(limits, width=50)
+	dev.off()
