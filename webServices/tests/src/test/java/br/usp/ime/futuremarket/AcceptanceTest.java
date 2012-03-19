827ad6c29175ec34fbe6722c97e6f498d88d9d57
Eduardo Kuroda <eduardohideo@gmail.com>
2012-03-19 12:03:52 -0300

    updated tests

--- webServices/tests/src/test/java/br/usp/ime/futuremarket/AcceptanceTest.java
+++ webServices/tests/src/test/java/br/usp/ime/futuremarket/AcceptanceTest.java
@@ -2,6 +2,10 @@ package br.usp.ime.futuremarket;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,76 +14,123 @@ import br.usp.ime.futuremarket.models.LowestPrice;
 public class AcceptanceTest {
     private static Customer customer;
     private LowestPrice list;
-
+	private static Set<ProductQuantity> products;
+	private static FutureMarket futureMarket;
+	private static CustomerInfo customerInfo;
+	
+	@BeforeClass
+	public static void initProductQuantity(){
+	      products = new HashSet<ProductQuantity>();
+	}
+	
+	@BeforeClass
+	public static void initCustomerInfo(){
+		customerInfo = new CustomerInfo();
+        customerInfo.setName("JUnit test");
+        customerInfo.setZipcode("01234-567");
+	}
+	
     @BeforeClass
     public static void createClients() {
-        final FutureMarket futureMarket = new FutureMarket();
+        futureMarket = new FutureMarket();
         customer = futureMarket.getFirstClient(FutureMarket.CUSTOMER_ROLE,
-                FutureMarket.CUSTOMER_SERVICE, Customer.class);
+
+        FutureMarket.CUSTOMER_SERVICE, Customer.class);
     }
 
     @Test
     // SM1
     public void product1LowestPriceShouldBe1() {
-        final String[] products = { "product1" };
+    	products = new HashSet<ProductQuantity>();
+        products.add(new ProductQuantity("product1",1));
         list = customer.getLowestPriceForList(products);
 
-        assertEquals(1d, list.getPrice(), 0.01d);
+        assertEquals(1.0d, list.getPrice(), 0.01d);
     }
 
     @Test
     // SM2
-    public void product2LowestPriceShouldBe2() {
-        final String[] products = { "product2" };
+    public void product2LowestPriceShouldBe10() {
+    	products = new HashSet<ProductQuantity>();
+        products.add(new ProductQuantity("product2",1));
         list = customer.getLowestPriceForList(products);
 
-        assertEquals(2d, list.getPrice(), 0.01d);
+        assertEquals(1.0d, list.getPrice(), 0.01d);
     }
 
     @Test
     // SM3
-    public void product3LowestPriceShouldBe3() {
-        final String[] products = { "product3" };
+    public void product3LowestPriceShouldBe1() {
+    	products = new HashSet<ProductQuantity>();
+        products.add(new ProductQuantity("product3",1));
         list = customer.getLowestPriceForList(products);
 
-        assertEquals(3d, list.getPrice(), 0.01d);
+        assertEquals(1.0d, list.getPrice(), 0.01d);
     }
 
     @Test
     // All SMs
     public void shouldAddProductPrices() {
-        final String[] products = { "product1", "product2", "product3" };
+    	products = new HashSet<ProductQuantity>();
+    	products.add(new ProductQuantity("product3",1));
+    	products.add(new ProductQuantity("product2",1));
+    	products.add(new ProductQuantity("product1",1));
         list = customer.getLowestPriceForList(products);
-
-        assertEquals(6d, list.getPrice(), 0.01d);
+        
+        assertEquals(1d+1d+1d, list.getPrice(),0.01d);
     }
 
     @Test
     public void clientShouldBeAbleToPurchase() {
-        final String[] products = { "product1", "product2", "product3" };
+    	products = new HashSet<ProductQuantity>();
+    	products.add(new ProductQuantity("product3",1));
+    	products.add(new ProductQuantity("product2",1));
+    	products.add(new ProductQuantity("product1",1));
         list = customer.getLowestPriceForList(products);
-
-        CustomerInfo customerInfo = new CustomerInfo();
-        customerInfo.setName("JUnit test");
-        customerInfo.setZipcode("01234-567");
-
+        
         final PurchaseInfo[] purchaseInfos = customer.makePurchase(list.getId(), customerInfo);
-
-        assertEquals(3, purchaseInfos.length);
+        assertEquals(1, purchaseInfos.length);
     }
 
     @Test
+    public void supermarketShouldBeAbleToRenewStock(){
+    	products = new HashSet<ProductQuantity>();
+    	products.add(new ProductQuantity("product1",200));
+    	list = customer.getLowestPriceForList(products);
+    	final PurchaseInfo[] purchaseInfos = customer.makePurchase(list.getId(), customerInfo);
+    	assertEquals(1, purchaseInfos.length);
+    }	
+    
+    @Test
     public void clientShouldBeAbleToGetShipmentData() {
-        final String[] products = { "product1", "product2", "product3" };
+    	products = new HashSet<ProductQuantity>();
+    	products.add(new ProductQuantity("product3",1));
+    	products.add(new ProductQuantity("product2",1));
+    	products.add(new ProductQuantity("product1",1));
         list = customer.getLowestPriceForList(products);
-
-        CustomerInfo customerInfo = new CustomerInfo();
-        customerInfo.setName("JUnit test");
-        customerInfo.setZipcode("01234-567");
-
+        
         final PurchaseInfo[] purchaseInfos = customer.makePurchase(list.getId(), customerInfo);
-
-        final DeliveryInfo deliveryInfo = customer.getShipmentData(purchaseInfos[0]);
+        final DeliveryInfo deliveryInfo = customer.getShipmentData(purchaseInfos[0]);        
         assertEquals("done", deliveryInfo.getStatus());
     }
+    
+    @Test
+    public void supermarket1ShouldBeAbleToPurchasefromSupplier1(){
+    	products = new HashSet<ProductQuantity>();
+    	products.add(new ProductQuantity("product1",1));
+    	final Supermarket supermarket1 = futureMarket.getClientByName("Supermarket1", FutureMarket.SUPERMARKET_SERVICE, Supermarket.class);
+    	
+    	PurchaseInfo purchase = supermarket1.purchase(products,customerInfo);
+		assertEquals(1,purchase.getProducts().size());
+    }
+    @Test
+    public void supplier1ShouldBeAbleToPurchasefromManufacture1(){
+    	products = new HashSet<ProductQuantity>();
+    	products.add(new ProductQuantity("product1",1));
+    	final Supermarket supplier1 = futureMarket.getClientByName("Supplier1", FutureMarket.SUPERMARKET_SERVICE, Supermarket.class);
+    	
+    	PurchaseInfo purchase = supplier1.purchase(products,customerInfo);
+    	assertEquals(1,purchase.getProducts().size());
+    }
+    
 }
