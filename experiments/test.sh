07d6bc7daa199d082073199c3d5a588b124951ee
Paulo Moura <pbmoura@gmail.com>
2012-03-28 11:07:11 -0300

    fix in test.sh

--- experiments/test.sh
+++ experiments/test.sh
@@ -4,7 +4,7 @@ matrix=""
 index=0
 for threads in 050 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800; do
 	echo -n "t$threads=( START_TSTAMP=$(date +%s)"
-	java -jar target/load-generator-1.0-jar-with-dependencies.jar $threads 2500 > test.log 2>&1
+	java -jar load-generator-1.0-jar-with-dependencies.jar $threads 2500 > test.log 2>&1
 	echo " END_TSTAMP=$(date +%s) THREADS=$threads )"
 	mv shipment.log lowest_price.log purchase.log data/$1/time/$threads/
 	matrix="$matrix\t[$index]=\"t$threads[*]\"\n"
