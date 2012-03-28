a03308dd016cc5819dd280689388de8d1affbaf5
Paulo Moura <pbmoura@gmail.com>
2012-03-27 22:21:23 -0300

    sar.sh updated to create files per number of threads

--- experiments/sar.sh
+++ experiments/sar.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-START_TSTAMP=0123456789
-END_TSTAMP=0123456789
+t1=( START_TSTAMP=1234567890 END_TSTAMP=1234567890 THREADS=1 )
+t2=( START_TSTAMP=1234567890 END_TSTAMP=1234567890 THREADS=2 )
+t3=( START_TSTAMP=1234567890 END_TSTAMP=1234567890 THREADS=3 )
+t5=( START_TSTAMP=1234567890 END_TSTAMP=1234567890 THREADS=5 )
+t8=( START_TSTAMP=1234567890 END_TSTAMP=1234567890 THREADS=8 )
 
-DAY=$(date -d @$START_TSTAMP +%d)
-START=$(date -d @$START_TSTAMP +%H:%M:%S)
-END=$(date -d @$END_TSTAMP +%H:%M:%S)
+matrix=( 
+	[0]="t1[*]"
+	[1]="t2[*]"
+	[2]="t3[*]"
+	[3]="t5[*]"
+	[4]="t8[*]"
+)
 
-# Received and Transmitted kB/sec
-sar -n DEV -f /var/log/sysstat/sa$DAY -s $START -e $END | grep eth0 | grep -v ^Average | tr -s ' ' | cut -f 1,6,7 -d ' '> net_sar.out
+ROOT_DIR=$1
 
-# Idle CPU (from 0 to 100)
-sar -P ALL -f /var/log/sysstat/sa$DAY -s $START -e $END | grep ^[0-9] | grep -v all | grep -v idle | tr -s ' ' | cut -f 1,3,9 -d ' ' > cpu_sar.out
+function createFiles() {
+	
+	local ${!1}
+	DAY=$(date -d @$START_TSTAMP +%d)
+	START=$(date -d @$START_TSTAMP +%H:%M:%S)
+	END=$(date -d @$END_TSTAMP +%H:%M:%S)	
+	DIR=$ROOT_DIR/$THREADS	
+	
+	# Received and Transmitted kB/sec
+	sar -n DEV -f /var/log/sysstat/sa$DAY -s $START -e $END | grep eth0 | grep -v ^Average | tr -s ' ' | cut -f 1,6,7 -d ' '> $DIR/net_sar.out
 
-# Memory usage (used, buffer, cache)
-sar -r -f /var/log/sysstat/sa$DAY -s $START -e $END | grep ^[0-9] | grep -v  kbm | tr -s ' ' | cut -f 1,4,6,7 -d ' ' > mem_sar.out
+	# Idle CPU (from 0 to 100)
+	sar -P ALL -f /var/log/sysstat/sa$DAY -s $START -e $END | grep ^[0-9] | grep -v all | grep -v idle | tr -s ' ' | cut -f 1,3,9 -d ' ' > $DIR/cpu_sar.out
+
+	# Memory usage (used, buffer, cache)
+	sar -r -f /var/log/sysstat/sa$DAY -s $START -e $END | grep ^[0-9] | grep -v  kbm | tr -s ' ' | cut -f 1,4,6,7 -d ' ' > $DIR/mem_sar.out
+}
+
+for i in ${matrix[*]}; do
+	createFiles $i 
+done
