dde3cfa4634e2b51a4683a17ab2dd3b93b44a3d2
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2012-05-18 10:41:24 -0300

    Fixed find params

--- webServices/test/scripts/deploy_test.sh
+++ webServices/test/scripts/deploy_test.sh
@@ -28,12 +28,12 @@ then
     echo "backup supermarkets"
     if [ -d "${SMSDIR}" ]
 	then
-	    find $SMSDIR -depth 1 -not -name \*.sh -exec cp -r {} $BACKUPDIR/supermarkets/ \; 
+	    find $SMSDIR -mindepth 1 -maxdepth 2 -not -name \*.sh -exec cp -r {} $BACKUPDIR/supermarkets/ \;
     fi
     echo "backup shippers"
     if [ -d "${SHIPPERSDIR}" ]
 	then
-	    find $SHIPPERSDIR -depth 1 -not -name \*.sh -exec cp -r {} $BACKUPDIR/shippers/ \;
+	    find $SHIPPERSDIR -mindepth 1 -maxdepth 2 -not -name \*.sh -exec cp -r {} $BACKUPDIR/shippers/ \;
     fi
     echo "backup futureMarket config"
     #futuremarket config backup
@@ -47,11 +47,11 @@ echo "applying test configurations"
 case $TESTCASE in
 [1-3])
     #copy supermarkets dir
-    find $SMSDIR -depth 1 -not -name \*.sh | xargs rm -rf 
+    find $SMSDIR -mindepth 1 -maxdepth 2 -not -name \*.sh | xargs rm -rf
     cp -r ${BASEDIR}/$TESTCASE/supermarkets/* ${SMSDIR}
 
     #copy shippers dir
-        find $SHIPPERSDIR -depth 1 -not -name \*.sh | xargs rm -rf 
+        find $SHIPPERSDIR -mindepth 1 -maxdepth 2 -not -name \*.sh | xargs rm -rf
     cp -r ${BASEDIR}/$TESTCASE/shippers/* ${SHIPPERSDIR}
 
     #copy futuremarket config
