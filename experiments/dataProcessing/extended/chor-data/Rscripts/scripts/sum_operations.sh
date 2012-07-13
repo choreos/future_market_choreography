219228ad00672d08df5ed5b4a3f9519c0eca7c0d
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2012-07-13 19:20:53 -0300

    Removed files other than simple experiment ones

--- experiments/dataProcessing/extended/chor-data/Rscripts/scripts/sum_operations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for thread in ../data/*/time/*; do
-  if test -e $thread/lowest_price.log; then
-    cat $thread/lowest_price.log $thread/purchase.log $thread/shipment.log | \
-    cut -f-2 -d' ' | sort -n >$thread/all_operations.log
-    echo $thread/all_operations.log
-  fi
-done
--- generator/config.rb
+++
@@ -1,9 +0,0 @@
-# Manual set up of environment variables
-PETALS_PLATAFORM_DIR="/home/besson/petals/petals-platform-3.1.3"
-NUMBER_PRODUCTS=1000
-
-# Auto set up of environment variables
-CXF_DIR_BIN=ENV['CXF_HOME']+"/bin"
-raise "Please set CXF_HOME environment variable." if CXF_DIR_BIN.nil?
-ROOT_DIR=(`pwd`).chomp
-$:.unshift File.dirname(__FILE__)
