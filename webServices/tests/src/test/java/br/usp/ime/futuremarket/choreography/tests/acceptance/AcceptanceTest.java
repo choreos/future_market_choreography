e0c6ef9899355997350cd6953549a2ddcbc02d9e
Paulo Moura <pbmoura@gmail.com>
2012-08-16 12:43:02 -0300

    created acceptance test for purchase operation

--- webServices/tests/src/test/java/br/usp/ime/futuremarket/choreography/tests/acceptance/AcceptanceTest.java
+++ webServices/tests/src/test/java/br/usp/ime/futuremarket/choreography/tests/acceptance/AcceptanceTest.java
@@ -3,10 +3,14 @@ package br.usp.ime.futuremarket.choreography.tests.acceptance;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import br.usp.ime.futuremarket.CustomerInfo;
 import br.usp.ime.futuremarket.Product;
+import br.usp.ime.futuremarket.Purchase;
 import br.usp.ime.futuremarket.ShopList;
 import br.usp.ime.futuremarket.ShopListItem;
 import br.usp.ime.futuremarket.choreography.Broker;
@@ -20,12 +24,20 @@ import br.usp.ime.futuremarket.choreography.ServiceName;
  * @author cadu
  * 
  */
+
 public class AcceptanceTest {
+	
+	private static final FutureMarket market = new FutureMarket();
+	private Broker broker;
+	
+	@Before
+	public void setup() throws Exception {
+		broker = market.getClientByRole(Role.BROKER, ServiceName.BROKER, Broker.class);
+	}
+	
     @Test
     public void testLowestPriceList() throws IOException {
-        final FutureMarket market = new FutureMarket();
-        final Broker broker = market.getClientByRole(Role.BROKER, ServiceName.BROKER, Broker.class);
-        final ShopList list = getShopList();
+    	final ShopList list = getShopList(false);
         final ShopList cheapList = broker.getLowestPrice(list);
 
         assertEquals(5, cheapList.getShopListItems().size());
@@ -40,14 +52,36 @@ public class AcceptanceTest {
         }
     }
 
-    // TODO Test other two operations
+    @Test
+    public void testPurchase() throws Exception {
+    	final CustomerInfo customer = getCustomerInfo();
+    	final ShopList list = getShopList(true);
+    	final Set<Purchase> purchases = broker.purchase(list, customer);
+    	assertEquals(5, purchases.size());
+    	
+    	int numberOfProducts;
+    	for(Purchase purchase : purchases) {    		
+    		numberOfProducts = purchase.getShopList().getProducts().size();
+    		assertEquals(1, numberOfProducts);
+    	}
+    }
+
+    private CustomerInfo getCustomerInfo() {
+		CustomerInfo customer = new CustomerInfo();
+		customer.setName("name");
+		customer.setAddress("adderss");
+		customer.setCreditCard("1111111111111111");
+		return customer;
+	}
+
+	// TODO Test other two operations
 
     private String getCheapestSm(final Product product) {
         final String prodName = product.getName();
         return prodName.replaceFirst("product", "supermarket");
     }
 
-    private ShopList getShopList() {
+    private ShopList getShopList(boolean setSupermerket) throws IOException {
         final ShopList list = new ShopList();
         Product product;
         ShopListItem item;
@@ -55,6 +89,8 @@ public class AcceptanceTest {
         for (int i = 1; i < 6; i++) {
             product = getProduct(i);
             item = getItem(product);
+            if (setSupermerket)
+            	item.setSeller(market.getBaseAddress(getCheapestSm(product)));
             list.put(item);
         }
 
