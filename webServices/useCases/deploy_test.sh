eba3649bc1cf5de0cfb455a538c130a19181d6c0
Paulo Moura <pbmoura@gmail.com>
2012-08-16 12:29:45 -0300

    fixed config.properties.template copy

--- webServices/useCases/deploy_test.sh
+++ webServices/useCases/deploy_test.sh
@@ -60,7 +60,7 @@ case $TESTCASE in
     find $SHIPPERSDIR -maxdepth 1 -mindepth 1 -not -name \*.sh | xargs rm -rf
     cp -r ${BASEDIR}/$TESTCASE/shippers/* ${SHIPPERSDIR}
 
-    cp ${BASEDIR}/$TESTCASE/config.properties ${FUTUREDIR}/src/main/resources/
+    cp ${BASEDIR}/$TESTCASE/config.properties.template ${FUTUREDIR}/src/main/resources/
 ;;
 *)
     echo "Please insert a valid test case."
