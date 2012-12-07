dd179743d4c185bdfcf5122f14453a57071b8194
Paulo Moura <pbmoura@gmail.com>
2012-12-07 18:34:47 -0200

    including scalability tests

---
+++ webServices/tests/src/test/java/br/usp/ime/futuremarket/tests/scalability/distributed/Enacter.java
@@ -0,0 +1,150 @@
+package br.usp.ime.futuremarket.tests.scalability.distributed;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ow2.choreos.enactment.ChoreographyNotFoundException;
+import org.ow2.choreos.enactment.EnactmentException;
+import org.ow2.choreos.enactment.client.EnactEngClient;
+import org.ow2.choreos.enactment.datamodel.ChorServiceSpec;
+import org.ow2.choreos.enactment.datamodel.ChorSpec;
+import org.ow2.choreos.enactment.datamodel.Choreography;
+import org.ow2.choreos.enactment.datamodel.ServiceDependence;
+import org.ow2.choreos.servicedeployer.datamodel.ArtifactType;
+import org.ow2.choreos.servicedeployer.datamodel.Service;
+import org.ow2.choreos.servicedeployer.datamodel.ServiceType;
+
+import br.usp.ime.futuremarket.AbstractWSInfo;
+import br.usp.ime.futuremarket.Role;
+import br.usp.ime.futuremarket.choreography.WSInfo;
+
+public class Enacter {
+
+    private static final String EE_HOST = "http://localhost:9102/enactmentengine";
+//    private static final String REPO = "http://www.linux.ime.usp.br/~cadu/futuremarket/";
+    private static final String REPO = "file://home/paulo/bin/apache-tomcat-7.0.25/webapps";
+    private static final String REG_NAME = "registry";
+    private static final String REG_ROLE = Role.REGISTRY.toString();
+    
+    private Map<String, String> deployedServices = new HashMap<String, String>();
+
+    public void enact() throws EnactmentException, ChoreographyNotFoundException {
+        final ChorSpec chorSpec = getChorSpec();
+
+        final EnactEngClient eeClient = new EnactEngClient(EE_HOST);
+        final String chorId = eeClient.createChoreography(chorSpec);
+        final Choreography chor = eeClient.enact(chorId);
+        for(Service service: chor.getDeployedServices()) {
+        	deployedServices.put(service.getName(), service.getUri());
+        	System.out.println(service.getUri());
+        }
+    }
+
+    private ChorSpec getChorSpec() {
+        final ChorSpec spec = new ChorSpec();
+
+//        spec.addServiceSpec(getServiceSpec("bank"));
+//
+//        spec.addServiceSpec(getServiceSpec("manufacturer"));
+//        spec.addServiceSpec(getServiceSpec("portal1"));
+//        spec.addServiceSpec(getServiceSpec("portal2"));
+//        spec.addServiceSpec(getServiceSpec("portal3"));
+//        spec.addServiceSpec(getServiceSpec("portal4"));
+//
+        spec.addServiceSpec(getServiceSpec(REG_NAME));
+//
+//        spec.addServiceSpec(getServiceSpec("shipper1"));
+//        spec.addServiceSpec(getServiceSpec("shipper2"));
+//
+//        spec.addServiceSpec(getServiceSpec("supermarket1"));
+//        spec.addServiceSpec(getServiceSpec("supermarket2"));
+//        spec.addServiceSpec(getServiceSpec("supermarket3"));
+//        spec.addServiceSpec(getServiceSpec("supermarket4"));
+//        spec.addServiceSpec(getServiceSpec("supermarket5"));
+//
+//        spec.addServiceSpec(getServiceSpec("supplier1"));
+//        spec.addServiceSpec(getServiceSpec("supplier2"));
+//        spec.addServiceSpec(getServiceSpec("supplier3"));
+
+        return spec;
+    }
+    
+    private ChorSpec getPortalSpec(int index) {
+    	final ChorSpec spec = new ChorSpec();
+    	
+    	spec.addServiceSpec(getPortalSpec("portal"+index));
+    	spec.addServiceSpec(getLegacyRegistrySpec(REG_NAME));
+    	
+    	return spec;
+    }
+
+    private ChorServiceSpec getLegacyRegistrySpec(String name) {
+    	final ChorServiceSpec service = new ChorServiceSpec();
+        service.setName(name);
+        service.setEndpointName(deployedServices.get(REG_NAME));
+        service.setPort(8080);
+        service.setType(ServiceType.SOAP);
+        service.setArtifactType(ArtifactType.LEGACY);
+        service.getRoles().add(getRole(name));
+        
+        return service;
+	}
+
+	private ChorServiceSpec getPortalSpec(String name) {
+		final ChorServiceSpec service = new ChorServiceSpec();
+        service.setName(name);
+        service.setCodeUri(REPO + name + ".war");
+        service.setEndpointName(getEndpoint(name));
+        service.setPort(8080);
+        service.setType(ServiceType.SOAP);
+        service.setArtifactType(ArtifactType.TOMCAT);
+        service.getRoles().add(getRole(name));
+
+        addDependencies(name, service);
+
+        return service;
+	}
+
+
+	private ChorServiceSpec getServiceSpec(final String name) {
+        final ChorServiceSpec service = new ChorServiceSpec();
+        service.setName(name);
+        service.setCodeUri(REPO + name + ".war");
+        service.setEndpointName(getEndpoint(name));
+        service.setPort(8080);
+        service.setType(ServiceType.SOAP);
+        service.setArtifactType(ArtifactType.TOMCAT);
+        service.getRoles().add(getRole(name));
+
+        addDependencies(name, service);
+
+        return service;
+    }
+
+    private void addDependencies(final String name, final ChorServiceSpec service) {
+        if (!REG_NAME.equals(name)) {
+            final ServiceDependence dep = new ServiceDependence();
+            dep.setServiceName(REG_NAME);
+            dep.setServiceRole(REG_ROLE);
+            service.getDependences().add(dep);
+        }
+    }
+
+    private String getRole(final String name) {
+        final AbstractWSInfo info = new WSInfo();
+        info.setName(name);
+        return info.getRole().toString();
+    }
+
+    private String getEndpoint(final String name) {
+        String endpoint = name;
+
+        if (REG_NAME.equals(name)) {
+            endpoint += "/endpoint";
+        } else {
+            endpoint += "/choreography";
+        }
+
+        return endpoint;
+    }
+}
---
+++ webServices/tests/src/test/java/br/usp/ime/futuremarket/tests/scalability/distributed/EnactmentSupermarketScalabilityTester.java
@@ -0,0 +1,141 @@
+package br.usp.ime.futuremarket.tests.scalability.distributed;
+
+import java.io.IOException;
+
+import org.apache.xmlbeans.XmlException;
+
+import eu.choreos.vv.ScalabilityTester;
+import eu.choreos.vv.aggregations.Mean;
+import eu.choreos.vv.clientgenerator.Item;
+import eu.choreos.vv.clientgenerator.ItemImpl;
+import eu.choreos.vv.clientgenerator.WSClient;
+import eu.choreos.vv.exceptions.FrameworkException;
+import eu.choreos.vv.exceptions.InvalidOperationNameException;
+import eu.choreos.vv.exceptions.WSDLException;
+import eu.choreos.vv.increasefunctions.LinearIncrease;
+import eu.choreos.vv.loadgenerator.UniformLoadGenerator;
+
+public class EnactmentSupermarketScalabilityTester extends ScalabilityTester {
+
+	private static final String REGISTRY_WSDL = "http://localhost:8080/registry/endpoint?wsdl";
+
+	private static final String[] portals = {
+			"http://localhost:8080/portal1/choreography?wsdl",
+			"http://localhost:8080/portal2/choreography?wsdl",
+			"http://localhost:8080/portal3/choreography?wsdl",
+			"http://localhost:8080/portal4/choreography?wsdl",
+			"http://localhost:8080/portal5/choreography?wsdl" };
+
+	private static final String ENDPOINT = "/choreography?wsdl";
+
+	private static final String[] productNames = { "bread", "milk", "beer",
+			"butter", "juice", "ham", "wine", "jam", "sugar" };
+
+	private WSClient registry;
+	private WSClient client;
+
+	private void includePortal(int index) throws WSDLException, XmlException,
+			IOException, FrameworkException, InvalidOperationNameException {
+		WSClient client = new WSClient(portals[index - 1]);
+		client.request("register");
+	}
+
+	private void removeAllPortals() throws Exception {
+		for (String wsdl : portals) {
+			removePortal(wsdl);
+		}
+	}
+
+	private void removePortal(String wsdl) throws Exception {
+		WSClient client;
+		client = new WSClient(wsdl);
+		client.request("unregister");
+	}
+	
+	private Item getLowestPrice() throws InvalidOperationNameException,
+	FrameworkException {
+		return client.request("getLowestPrice", getProductList());
+	}
+	
+	private void purchase(Item items) throws InvalidOperationNameException,
+	FrameworkException {
+		client.request("purchase", getShopList(items));
+	}
+	
+	private Item getShopList(Item purchaseItems) {
+		Item purchase = new ItemImpl("purchase");
+		Item arg1 = purchase.addChild("arg1");
+		arg1.addChild("creditCard").setContent("0000000000");
+		arg1.addChild("address").setContent("my home...");
+		arg1.addChild("name").setContent("john");
+		Item arg0 = purchase.addChild("arg0");
+		arg0.addChild(purchaseItems);
+		
+		return purchase;
+	}
+	
+	private Item getProductList() {
+		final Item getLowestPrice = new ItemImpl("getLowestPrice");
+		Item arg0 = getLowestPrice.addChild("arg0");
+		Item items = arg0.addChild("items");
+		Item entry = items.addChild("entry");
+		entry.addChild("value").setContent(someQuantity());
+		entry.addChild("key").setContent(someProduct());
+		
+		return getLowestPrice;
+	}
+	
+	private String someProduct() {
+		final int index = (int) Math.floor(Math.random() * productNames.length);
+		return productNames[index];
+	}
+	
+	private String someQuantity() {
+		return "" + Math.round(Math.random() * 10);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		removeAllPortals();
+		registry = new WSClient(REGISTRY_WSDL);
+	}
+
+	@Override
+	public void resourceScaling(int resourceQuantity) throws WSDLException,
+			XmlException, IOException, FrameworkException,
+			InvalidOperationNameException {
+		includePortal(resourceQuantity);
+	}
+
+	@Override
+	public void beforeTest() throws Exception {
+		Item result = registry.request("getServiceRoundRobin", "portal");
+		String wsdl = result.getContent("return");
+		client = new WSClient(wsdl + ENDPOINT);
+	}
+
+	@Override
+	public void test() throws Exception {
+		Item returnItem = getLowestPrice();
+		purchase(returnItem.getChild("return").getChild("items"));
+	}
+
+	public static void main(String[] args) {
+		EnactmentSupermarketScalabilityTester tester = new EnactmentSupermarketScalabilityTester();
+
+		tester.setAggregator(new Mean());
+		tester.setScalabilityFunction(new LinearIncrease());
+		tester.setLoadGenerator(new UniformLoadGenerator());
+
+		tester.setInititalResoucesQuantity(1);
+		tester.setInitialRequestsPerMinute(300);
+		tester.setNumberOfSteps(5);
+		tester.setNumberOfExecutionsPerStep(10);
+		try {
+			tester.run("get price and purchase");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		tester.showChart("supermarket choreography");
+	}
+}
